702a09fb1cac54310cfffcb4512ef17ffc5f7003
GabinBonnier <bonnier.gabin@yahoo.com>
2026-02-05 15:21:24 +0100

    Rectification du bug du test automatique

--- src/api/cards/cards.routes.ts
+++ src/api/cards/cards.routes.ts
@@ -3,7 +3,7 @@ import {prisma} from '../../database'
 
 const router = Router()
 
-router.get('/', async (req: Request, res: Response) => {
+router.get('/', async (_req: Request, res: Response) => {
     try {
         const cards = await prisma.card.findMany({
             orderBy: {
