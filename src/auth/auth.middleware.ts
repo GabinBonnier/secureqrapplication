756cdd8652d4cead8735b847d92b8d7f381bf8f4
GabinBonnier <bonnier.gabin@yahoo.com>
2026-02-05 09:50:58 +0100

    Correction bug test automatique github

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -1,25 +1,38 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import {NextFunction, Request, Response} from 'express'
+import jwt from 'jsonwebtoken'
 
 declare global {
-  namespace Express {
-    interface Request {
-      userId?: number;
+    namespace Express {
+        interface Request {
+            userId?: number
+        }
     }
-  }
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization;
-  const token = authHeader && authHeader.split(' ')[1];
+export const authenticateToken = (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    const authHeader = req.headers.authorization
+    const token = authHeader && authHeader.split(' ')[1] // Format: "Bearer TOKEN"
 
-  if (!token) return res.status(401).json({ error: 'Token manquant' });
+    if (!token) {
+      return res.status(401).json({error: 'Token manquant'})
+    }
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { userId: number; email: string };
-    req.userId = decoded.userId;
-    next();
-  } catch {
-    return res.status(403).json({ error: 'Token invalide ou expiré' });
-  }
-};
+    try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
+        userId: number
+        email: string
+    }
+
+    req.user = {
+      userId: decoded.userId,
+      email: decoded.email
+    }
+    return next()
+    } catch (error) {
+      return res.status(401).json({error: 'Token invalide ou expiré'})
+    }
+}
