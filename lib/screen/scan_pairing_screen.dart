93777e684989b35ee224b73795cf5d0779971182
GabinBonnier <bonnier.gabin@yahoo.com>
2026-02-10 16:20:23 +0100

    Update pour ajout de bouton pour la modif

---
+++ lib/screen/scan_pairing_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'scan_pairing_screen.dart';
+
+class ScanPairingScreen extends StatelessWidget {
+  const ScanPairingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Initialisation'),
+      ),
+      body: Center(
+      ),
+    );
+  }
+}
